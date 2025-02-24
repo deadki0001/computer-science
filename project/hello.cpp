5a8ed19650b390a1cc6d55abc00859ca462f1e68
Devon Adkins <adkins.devon@hotmail.com>
2025-02-24 21:35:17 +0200

    first C++ project

---
+++ project/hello.cpp
@@ -0,0 +1,9 @@
+// Name: Hello World App
+
+#include <iostream> // instruction to fetch a file called iostream - to take a copy of the file before compiling a program.
+using namespace std; // statements must finish in a semi-colon
+
+int main() {  // int is a type declaration
+    cout << "Hello C++ You are definitely not PYTHON!" << endl; // cout is an object // << operator insertion // double quotes form a string // endl - stands for ends line
+    return 0; // zero is a code returned to the operating system to state everything is ok.
+}
